ca2aa0c83a47fad19ae2e7f5d997884e6b9b78b2
Gabriel Barone <gbarone2004@gmail.com>
2018-03-31 15:51:46 -0300

    Update install.py

--- install.py
+++ install.py
@@ -1,13 +1,13 @@
-import sys, os, platform, pip
-quest = raw_input('Do you want to make the program colorful (Y/N): ')
-if quest.lower() == 'y':
+import sys, os, platform, pip, time
+time.sleep(1.5)
+print('[*] Creating output dir...\n')
+if not os.path.isdir('output'):
     os.mkdir('output')
-    print '[+] Creating output dir...\n'
-    pip.main(['install', 'colorama'])
-    print '[*] Colorama sucessfuly installed!'
-    print 'You can now run the file "l.py"'
-else:
-    os.mkdir('output')
-    print '[+] Creating output dir...\n'
-    print 'You can execute "l.py" with no colors'
-    print 'Exiting...'
+print('[+] Output dir created, installing modules...\n')
+pip.main(['install', 'colorama'])
+print('[*] Colorama sucessfuly installed!')
+pip.main(['install', 'requests'])
+print('[*] Requests sucessfuly installed!')
+print('[+] LazyPie Sucessfuly installed!')
+print('You can execute "l.py" now!')
+print('Exiting...')
